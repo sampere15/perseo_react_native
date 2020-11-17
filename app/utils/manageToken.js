1a5c32203fb68a197e5b248cafbb83e197fa3f74
Adrián Sampere <sampere.adrian@gmail.com>
2020-11-17 09:11:13 +0100

    Created helper to manage auth token. Action to download data from the server and store it in Redux

---
+++ app/utils/manageToken.js
@@ -0,0 +1,18 @@
+import * as SecureStore from 'expo-secure-store';
+
+const TOKEN = "token";
+
+export default manageToken = {
+  //  To store token in the SecureStore
+  storeTokenSecureStore: async (token) => {
+    return await SecureStore.setItemAsync(TOKEN, token);
+  },
+  //  Recover token from SecureStore
+  getTokenSecureStore: async () => {
+    return await SecureStore.getItemAsync(TOKEN);
+  },
+  //  Delete token from SecureStore
+  deleteTokenSecureStore: async () => {
+    return await SecureStore.deleteItemAsync(TOKEN);
+  },
+};
