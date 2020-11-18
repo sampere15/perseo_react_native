829387f12d6c63309f971a586b514bb28469e498
Adrián Sampere <sampere.adrian@gmail.com>
2020-11-18 18:08:44 +0100

    Now we download after login

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { View, ScrollView, Text, FlatList, StyleSheet, ActivityIndicator } from "react-native";
-import Loading from "../components/loading/Loading";
 import MediaList from "../components/media/MediaList";
 
 //  Redux
@@ -8,25 +7,10 @@ import { useSelector, useDispatch } from "react-redux";
 import { getFilesAction } from "../redux/actions/mediaActions";
 
 export default function Home({navigation}) {
-  const dispatch = useDispatch();
-  const { downloading, files, user } = useSelector( state => state.media);
-  let favourites = [];
-  let last = [];
-  
-  //  Filter the data to pass them to the MediaList component
-  if(user) {
-    favourites = files.filter( file => user.favs.includes(file.id));
-    last = files.filter( file => user.lastShowed.includes(file.id));
-  }
-
-  //  Request for data to the API
-  useEffect( () => {
-    dispatch(getFilesAction());
-  }, []);
-
-  if(downloading) {
-    return <ActivityIndicator style={styles.activityIndicator} size="large" color="#ccc" />
-  } 
+  // const dispatch = useDispatch();
+  const { files, user } = useSelector( state => state.media);
+  const favourites = files.filter( file => user.favs.includes(file.id));
+  const last = files.filter( file => user.lastShowed.includes(file.id));
 
   return (
     <ScrollView style={styles.container}>
@@ -47,7 +31,6 @@ export default function Home({navigation}) {
         title="Content"
         files={files}
       />
-      <Loading isVisible={downloading} text="Downloading data..." />
     </ScrollView>
   );
 };
